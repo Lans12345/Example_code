773b43b4e50158016a4d6e7318df1c33904313bf
Lans12345 <olanalans12345@gmail.com>
2022-12-22 11:45:49 +0800

    wip providers home page

--- lib/screens/providers/provider_home.dart
+++ lib/screens/providers/provider_home.dart
@@ -1,8 +1,64 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:the_serve_new/auth/providers/provider_login.dart';
+import 'package:the_serve_new/widgets/text_widget.dart';
 
 class ProviderHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            onPressed: () async {
+              showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                        title: const Text(
+                          'Logout Confirmation',
+                          style: TextStyle(
+                              fontFamily: 'QBold', fontWeight: FontWeight.bold),
+                        ),
+                        content: const Text(
+                          'Are you sure you want to Logout?',
+                          style: TextStyle(fontFamily: 'QRegular'),
+                        ),
+                        actions: <Widget>[
+                          FlatButton(
+                            onPressed: () => Navigator.of(context).pop(true),
+                            child: const Text(
+                              'Close',
+                              style: TextStyle(
+                                  fontFamily: 'QRegular',
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          FlatButton(
+                            onPressed: () async {
+                              await FirebaseAuth.instance.signOut();
+                              Navigator.of(context).pushReplacement(
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const ProviderLogin()));
+                            },
+                            child: const Text(
+                              'Continue',
+                              style: TextStyle(
+                                  fontFamily: 'QRegular',
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
+                      ));
+            },
+            icon: const Icon(Icons.logout, color: Colors.white),
+          ),
+        ],
+        backgroundColor: Colors.blue,
+        title: TextRegular(text: 'Home', fontSize: 18, color: Colors.white),
+        centerTitle: true,
+      ),
+    );
   }
 }
