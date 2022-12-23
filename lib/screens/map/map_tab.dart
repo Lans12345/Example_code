2c593cf30f389d47c354119961dc0726ee869123
Lans12345 <olanalans12345@gmail.com>
2022-12-23 11:31:40 +0800

    wip fetching locations

--- lib/screens/map/map_tab.dart
+++ lib/screens/map/map_tab.dart
@@ -1,19 +1,12 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../../widgets/text_widget.dart';
 
-class MapTab extends StatelessWidget {
-  final Completer<GoogleMapController> _controller =
-      Completer<GoogleMapController>();
-
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
+class MapTab extends StatefulWidget {
   static const CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
       target: LatLng(37.43296265331129, -122.08832357078792),
@@ -21,18 +14,62 @@ class MapTab extends StatelessWidget {
       zoom: 19.151926040649414);
 
   @override
+  State<MapTab> createState() => _MapTabState();
+}
+
+class _MapTabState extends State<MapTab> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  late double lat = 0;
+  late double long = 0;
+
+  var hasLoaded = false;
+  getLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    setState(() {
+      lat = position.latitude;
+      long = position.longitude;
+
+      hasLoaded = true;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    CameraPosition kGooglePlex = CameraPosition(
+      target: LatLng(lat, long),
+      zoom: 14.4746,
+    );
     return Column(
       children: [
-        Expanded(
-          child: GoogleMap(
-            mapType: MapType.normal,
-            initialCameraPosition: _kGooglePlex,
-            onMapCreated: (GoogleMapController controller) {
-              _controller.complete(controller);
-            },
-          ),
-        ),
+        hasLoaded
+            ? Expanded(
+                child: GoogleMap(
+                  myLocationEnabled: true,
+                  mapType: MapType.normal,
+                  initialCameraPosition: kGooglePlex,
+                  onMapCreated: (GoogleMapController controller) {
+                    _controller.complete(controller);
+                  },
+                ),
+              )
+            : const Center(
+                child: CircularProgressIndicator(),
+              ),
         Container(
           color: Colors.white30,
           height: 100,
